5c5efb4968c35e5e0e1ba1fbeac61e3fc01615bd
billi <billi@BILLIJO>
2026-05-28 16:29:33 -0700

    aptura de errores

--- Back/Rutas/Reportes/Obtener_reporte.py
+++ Back/Rutas/Reportes/Obtener_reporte.py
@@ -7,38 +7,43 @@ Obtener_reporte_bp = Blueprint('obtener_reporte', __name__)
 
 
 def obtener_reportes():
-    docs = firestore_db.collection('reportes').stream()
-    reportes = []
+    try:
+        docs = firestore_db.collection('reportes').stream()
+        reportes = []
 
-    for doc in docs:
+        for doc in docs:
 
-        data = doc.to_dict()
+            data = doc.to_dict()
 
-        reporte = Reporte(
-            id=doc.id,
-            descripcion=data.get('descripcion'),
-            imagen=data.get('url_imagen'),
-            fecha=data.get('fecha'),
-            ubicacion=data.get('ubicacion'),
-            estado=data.get('estado', 'normal')
-        )
+            reporte = Reporte(
+                id=doc.id,
+                descripcion=data.get('descripcion'),
+                imagen=data.get('url_imagen'),
+                fecha=data.get('fecha'),
+                ubicacion=data.get('ubicacion'),
+                estado=data.get('estado', 'normal')
+            )
 
-        reportes.append(reporte)
+            reportes.append(reporte)
 
-    return reportes
+        return reportes
+    except  Exception as e:
+        return jsonify({'Error':f'Error obteneindo los reportes {str(e)}'})
 
 
 @Obtener_reporte_bp.route('/obtener_reportes',methods=['GET'])
 def reportes():
+    try:
+        reportes = obtener_reportes()
+        tarjetas = []
 
-    reportes = obtener_reportes()
-    tarjetas = []
+        for reporte in reportes:
+            factory = FactoryTarjetas.crear_tarjeta(reporte)
 
-    for reporte in reportes:
-        factory = FactoryTarjetas.crear_tarjeta(reporte)
+            tarjeta = factory.crear_tarjeta()
 
-        tarjeta = factory.crear_tarjeta()
+            tarjetas.append(tarjeta)
 
-        tarjetas.append(tarjeta)
-
-    return jsonify(tarjetas), 200
+        return jsonify(tarjetas), 200
+    except Exception as e:
+        return jsonify({'Error':f'Error al obtener los reportes {str(e)}'}), 500
