3caf81c16e24a4e54d53a204586c295fbf48508f
billi <billi@BILLIJO>
2026-05-28 14:44:01 -0700

    inicio fabrica de reportes

---
+++ Back/Funciones/Fabrica_reportes.py
@@ -0,0 +1,12 @@
+class Reporte:
+
+    def __init__(self, id, descripcion, imagen, fecha, ubicacion,hora,urgencia):
+
+        self.id = id
+        self.descripcion = descripcion
+        self.imagen = imagen
+        self.fecha = fecha
+        self.hora = hora
+        self.ubicacion = ubicacion
+        self.estado = urgencia
+
